6ae942ca6265784af4ab7265e8930af7f195a48b
antiisolo <me@antiisolo.com>
2012-01-31 21:49:05 -0800

    Update php/photos.php

--- php/photos.php
+++ php/photos.php
@@ -1,3 +1,11 @@
 <?php
-
-	if (!$getfiles = op
+	if (!$checkfiles = opendir('./photos')) { //Point to any directory you want. Right now it's looking in photos
+		echo "Could not open directory.";
+		}
+		
+		while (FALSE !== ($getfiles = readdir($checkfiles))) {
+			if (strpos($getfiles, '.jpg')) { // Only looking for .jpgs, can add more formats
+			echo "<img src=\"/photos/". $getfiles . "\" /><br />"; //Outputs <img src= for every jpg in the directory
+			}
+}
+	?>
